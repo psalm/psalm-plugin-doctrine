d332d29fc3bd58088c2dc339c1c6f0ea482c2b7b
Bruce Weirdan <weirdan@gmail.com>
2018-11-03 14:57:16 +0200

    update entry point to match the interface

--- Plugin.php
+++ Plugin.php
@@ -1,11 +1,12 @@
 <?php
 namespace Weirdan\PsalmDoctrineCollections;
 use Psalm\PluginApi;
+use SimpleXMLElement;
 
 class Plugin implements PluginApi\PluginEntryPointInterface
 {
     /** @return void */
-    public function __invoke(PluginApi\RegistrationInterface $psalm)
+    public function __invoke(PluginApi\RegistrationInterface $psalm, ?SimpleXMLElement $config = null)
     {
         $psalm->addStubFile(__DIR__ . '/' . 'stubs/DoctrineCollections.php');
     }
