85f8386c4efac8d8ca67be7cc83ff944f6116922
Bruce Weirdan <weirdan@gmail.com>
2018-11-13 16:43:34 +0200

    Psalm API updates
    Additionally added psalm.xml.dist and phpcs.xml.dist

--- Plugin.php
+++ Plugin.php
@@ -1,12 +1,14 @@
 <?php
 namespace Weirdan\PsalmDoctrineCollections;
-use Psalm\PluginApi;
-use SimpleXMLElement;
 
-class Plugin implements PluginApi\PluginEntryPointInterface
+use SimpleXMLElement;
+use Psalm\Plugin\PluginEntryPointInterface;
+use Psalm\Plugin\RegistrationInterface;
+
+class Plugin implements PluginEntryPointInterface
 {
     /** @return void */
-    public function __invoke(PluginApi\RegistrationInterface $psalm, ?SimpleXMLElement $config = null)
+    public function __invoke(RegistrationInterface $psalm, ?SimpleXMLElement $config = null)
     {
         $psalm->addStubFile(__DIR__ . '/' . 'stubs/DoctrineCollections.php');
     }
