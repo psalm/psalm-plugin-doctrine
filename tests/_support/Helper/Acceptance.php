ee0bf802183293a578a4a8a9ffdd62e81ec6e266
Bruce Weirdan <weirdan@gmail.com>
2019-01-27 05:28:38 +0200

    Moved psalm test file out of the root folder

--- tests/_support/Helper/Acceptance.php
+++ tests/_support/Helper/Acceptance.php
@@ -6,10 +6,11 @@ namespace Weirdan\DoctrinePsalmPlugin\Tests\Helper;
 
 class Acceptance extends \Codeception\Module
 {
+    private const FILENAME = 'tests/_run/somefile.php';
     public function runPsalmWith(string $code): void
     {
-        $this->getModule('Filesystem')->writeToFile('somefile.php', $code);
-        $this->getModule('Cli')->runShellCommand('vendor/bin/psalm --output-format=pylint somefile.php', false);
+        $this->getModule('Filesystem')->writeToFile(self::FILENAME, $code);
+        $this->getModule('Cli')->runShellCommand('vendor/bin/psalm --output-format=pylint ' . self::FILENAME, false);
     }
 
     public function canSeePsalmFailsWith(string $message): void
