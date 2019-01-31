8df11b36be61081dc81d1a6b250be4d271d4c49f
Bruce Weirdan <weirdan@gmail.com>
2019-01-31 12:36:24 +0200

    class -> interface
    Also added explicit mixed type to $id param

--- stubs/EntityManagerInterface.php
+++ stubs/EntityManagerInterface.php
@@ -3,7 +3,7 @@ namespace Doctrine\ORM;
 
 use Doctrine\Common\Persistence\ObjectManager;
 
-class EntityManagerInterface extends ObjectManager
+interface EntityManagerInterface extends ObjectManager
 {
     /**
      * @template T
@@ -11,25 +11,27 @@ class EntityManagerInterface extends ObjectManager
      * @template-typeof T $entityName
      * @return EntityRepository<T>
      */
-    public function getRepository(string $entityName) {}
+    public function getRepository(string $entityName);
 
     /**
      * @template T
      * @param class-string $entityName
      * @template-typeof T $entityName
+     * @param mixed $id
      * @return ?T
      * @throws OptimisticLockException
      * @throws ORMInvalidArgumentException
      * @throws TransactionRequiredException
      * @throws ORMException
      */
-    public function find(string $entityName, $id, ?int $lockMode = null, ?int $lockVersion = null) {}
+    public function find(string $entityName, $id, ?int $lockMode = null, ?int $lockVersion = null);
 
     /**
      * @template T
      * @param class-string $entityName
      * @template-typeof T $entityName
+     * @param mixed $id
      * @return T
      */
-    public function getReference(string $entityName, $id) {}
+    public function getReference(string $entityName, $id);
 }
