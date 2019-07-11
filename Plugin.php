738a47dec9fdfadd89bedeedecca81e37c33542c
Bruce Weirdan <weirdan@gmail.com>
2019-07-12 01:53:53 +0300

    Add PackageVersions polyfill

--- Plugin.php
+++ Plugin.php
@@ -3,6 +3,7 @@ namespace Weirdan\DoctrinePsalmPlugin;
 
 use OutOfBoundsException;
 use PackageVersions\Versions;
+use Muglug\PackageVersions\Versions as LegacyVersions;
 use SimpleXMLElement;
 use Psalm\Plugin\PluginEntryPointInterface;
 use Psalm\Plugin\RegistrationInterface;
@@ -28,12 +29,36 @@ class Plugin implements PluginEntryPointInterface
     /** @return string[] */
     private function getBundleStubs(): array
     {
-        try {
-            Versions::getVersion('doctrine/doctrine-bundle');
-        } catch (OutOfBoundsException $e) {
+        if (!$this->hasPackage('doctrine/doctrine-bundle')) {
             return [];
         }
 
         return glob(__DIR__ . '/' . 'bundle-stubs/*.php');
+    }
+
+    private function hasPackage(string $packageName): bool
+    {
+        try {
+            $this->getPackageVersion($packageName);
+        } catch (OutOfBoundsException $e) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @throws OutOfBoundsException
+     */
+    private function getPackageVersion(string $packageName): string
+    {
+        if (class_exists(Versions::class)) {
+            return (string) Versions::getVersion($packageName);
+        }
+
+        if (class_exists(LegacyVersions::class)) {
+            return (string) LegacyVersions::getVersion($packageName);
+        }
+
+        throw new OutOfBoundsException;
     }
 }
