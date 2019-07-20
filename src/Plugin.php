ea1215078e83869b395694459f0f0560c458ec49
Bruce Weirdan <weirdan@gmail.com>
2019-07-20 13:10:23 +0300

    Disabled Collection stub for newer doctrine/collections versions
    Refs #21

--- src/Plugin.php
+++ src/Plugin.php
@@ -10,7 +10,12 @@ use SimpleXMLElement;
 
 use function array_merge;
 use function class_exists;
+use function explode;
 use function glob;
+use function preg_grep;
+use function version_compare;
+
+use const PREG_GREP_INVERT;
 
 class Plugin implements PluginEntryPointInterface
 {
@@ -27,10 +32,16 @@ class Plugin implements PluginEntryPointInterface
     /** @return string[] */
     private function getStubFiles(): array
     {
-        return array_merge(
-            glob(__DIR__ . '/../stubs/*.phpstub') ?: [],
-            glob(__DIR__ . '/../stubs/DBAL/*.phpstub') ?: []
-        );
+        $files = glob(__DIR__ . '/../stubs/*.phpstub') ?: [];
+
+        if ($this->hasPackage('doctrine/collections')) {
+            [$ver] = explode('@', $this->getPackageVersion('doctrine/collections'));
+            if (version_compare($ver, 'v1.6.0', '>=')) {
+                $files = preg_grep('/Collections\.phpstub$/', $files, PREG_GREP_INVERT);
+            }
+        }
+
+        return array_merge($files, glob(__DIR__ . '/../stubs/DBAL/*.phpstub') ?: []);
     }
 
     /** @return string[] */
