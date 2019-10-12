8c90cbb314ad99e5efbfc7533c194131f3d4e0e5
Demi <2e3s19@gmail.com>
2019-10-12 18:11:08 -0400

    Divide first argument and variadic arguments

--- stubs/QueryBuilder.php
+++ stubs/QueryBuilder.php
@@ -3,54 +3,58 @@
 namespace Doctrine\ORM;
 
 /**
- * @template WhereExpr as \Doctrine\ORM\Query\Expr\Base|list<\Doctrine\ORM\Query\Expr\Base|string>|string
+ * @template WhereExpr as \Doctrine\ORM\Query\Expr\Base|string
+ * @template WhereArg as \Doctrine\ORM\Query\Expr\Base|list<\Doctrine\ORM\Query\Expr\Base|string>|string
+ *
+ * @template SelectExpr as \Doctrine\ORM\Query\Expr\Func|string
+ * @template SelectArg as \Doctrine\ORM\Query\Expr\Func|list<\Doctrine\ORM\Query\Expr\Func|string>|string|null
  */
 class QueryBuilder
 {
     /**
-     * @param list<string>|string|null $select
-     * @param string ...$selects
+     * @param SelectArg $select
+     * @param SelectExpr ...$selects
      */
     public function select($select = null, ...$selects): self {}
 
     /**
-     * @param list<string>|string|null $select
-     * @param string ...$selects
+     * @param SelectArg $select
+     * @param SelectExpr ...$selects
      */
     public function addSelect($select = null, ...$selects): self {}
 
     /**
-     * @param WhereExpr $predicate
+     * @param WhereArg $predicate
      * @param WhereExpr ...$predicates
      */
     public function where($predicate = null, ...$predicates): self {}
 
     /**
-     * @param WhereExpr $predicate
+     * @param WhereArg $predicate
      * @param WhereExpr ...$predicates
      */
     public function andWhere($predicate = null, ...$predicates): self {}
 
     /**
-     * @param WhereExpr $predicate
+     * @param WhereArg $predicate
      * @param WhereExpr ...$predicates
      */
     public function orWhere($predicate = null, ...$predicates): self {}
 
     /**
-     * @param WhereExpr $predicate
+     * @param WhereArg $predicate
      * @param WhereExpr ...$predicates
      */
     public function having($predicate = null, ...$predicates): self {}
 
     /**
-     * @param WhereExpr $predicate
+     * @param WhereArg $predicate
      * @param WhereExpr ...$predicates
      */
     public function andHaving($predicate = null, ...$predicates): self {}
 
     /**
-     * @param WhereExpr $predicate
+     * @param WhereArg $predicate
      * @param WhereExpr ...$predicates
      */
     public function orHaving($predicate = null, ...$predicates): self {}
