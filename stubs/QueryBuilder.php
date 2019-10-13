56b7f12982d1df28cdd0ca3d98cb07f063dd1d6a
Bruce Weirdan <weirdan@gmail.com>
2019-10-13 04:49:49 +0300

    Replaced generics with typedefs
    - Replaced type arguments with typedefs as neither QueryBuilder nor its
    methods are really generic
    - Clarified QueryBuilder method signatures

--- stubs/QueryBuilder.php
+++ stubs/QueryBuilder.php
@@ -5,56 +5,56 @@ namespace Doctrine\ORM;
 use Doctrine\ORM\Query\Expr;
 
 /**
- * @template WhereExpr as Expr\Base|string
- * @template SelectExpr as Expr\Func|string
+ * @psalm-type _WhereExpr=Expr\Base|string
+ * @psalm-type _SelectExpr=Expr\Func|string
  */
 class QueryBuilder
 {
     /**
-     * @param string|null|array<array-key,string|Expr\Func>|Expr\Func $select
-     * @param SelectExpr ...$selects
+     * @param null|_SelectExpr|_SelectExpr[] $select
+     * @param _SelectExpr ...$selects
      */
     public function select($select = null, ...$selects): self {}
 
     /**
-     * @param string|null|array<array-key,string|Expr\Func>|Expr\Func $select
-     * @param SelectExpr ...$selects
+     * @param null|_SelectExpr|_SelectExpr[] $select
+     * @param _SelectExpr ...$selects
      */
     public function addSelect($select = null, ...$selects): self {}
 
     /**
-     * @param array<array-key,string|Expr\Base>|Expr\Base|string $predicate
-     * @param WhereExpr ...$predicates
+     * @param _WhereExpr|_WhereExpr[] $predicate
+     * @param _WhereExpr ...$predicates
      */
     public function where($predicate, ...$predicates): self {}
 
     /**
-     * @param array<array-key,string|Expr\Base>|Expr\Base|string $predicate
-     * @param WhereExpr ...$predicates
+     * @param _WhereExpr|_WhereExpr[] $predicate
+     * @param _WhereExpr ...$predicates
      */
     public function andWhere($predicate, ...$predicates): self {}
 
     /**
-     * @param array<array-key,string|Expr\Base>|Expr\Base|string $predicate
-     * @param WhereExpr ...$predicates
+     * @param _WhereExpr|_WhereExpr[] $predicate
+     * @param _WhereExpr ...$predicates
      */
     public function orWhere($predicate, ...$predicates): self {}
 
     /**
-     * @param array<array-key,string|Expr\Base>|Expr\Base|string $predicate
-     * @param WhereExpr ...$predicates
+     * @param _WhereExpr|_WhereExpr[] $predicate
+     * @param _WhereExpr ...$predicates
      */
     public function having($predicate, ...$predicates): self {}
 
     /**
-     * @param array<array-key,string|Expr\Base>|Expr\Base|string $predicate
-     * @param WhereExpr ...$predicates
+     * @param _WhereExpr|_WhereExpr[] $predicate
+     * @param _WhereExpr ...$predicates
      */
     public function andHaving($predicate, ...$predicates): self {}
 
     /**
-     * @param array<array-key,string|Expr\Base>|Expr\Base|string $predicate
-     * @param WhereExpr ...$predicates
+     * @param _WhereExpr|_WhereExpr[] $predicate
+     * @param _WhereExpr ...$predicates
      */
     public function orHaving($predicate, ...$predicates): self {}
 }
