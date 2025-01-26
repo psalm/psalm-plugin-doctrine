c41a652548b14490037d99e6d6a9e4e85c85b70d
Daniil Gentili <daniil@daniil.it>
2025-01-26 12:14:09 +0100

    Fix

--- src/Plugin.php
+++ src/Plugin.php
@@ -16,6 +16,9 @@ use function explode;
 use function glob;
 use function strpos;
 
+/**
+ * @api
+ */
 class Plugin implements PluginEntryPointInterface
 {
     public function __invoke(RegistrationInterface $psalm, ?SimpleXMLElement $config = null): void
